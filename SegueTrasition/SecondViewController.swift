5dd044a807da42b18dcdc7c413e2a6bfc7830886
402-07 <a402-07@402-07ui-imac.local>
2018-08-12 16:05:04 +0900

    code update

---
+++ SegueTrasition/SecondViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.swift
+//  SegueTrasition
+//
+//  Created by 402-07 o	n 2018. 8. 4..
+//  Copyright © 2018년 moonbc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+    @IBOutlet weak var label: UILabel!
+    
+    var msg:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        label.text = msg
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SegueTrasition/ViewController.swift
+++ SegueTrasition/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+//        let destination = segue.destination as! SecondViewController
+//        destination.msg  = "데이터 넘기기"
+    }
 
+    @IBAction func returned(segue:UIStoryboardSegue) {
+        if segue.identifier == "seg1" {
+            print("제자리로 돌아올 때 호출 됩니다.")
+        }else {
+            print("제자리로 돌아올 때 호출 됩니다.22222")
+        }
+    }
+    
 }
 
